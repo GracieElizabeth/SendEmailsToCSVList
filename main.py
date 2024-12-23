a5902c79866b69ecfe7de06f5b4651739398f539
Grace <gralatta@gmail.com>
2024-12-22 20:46:37 -0600

    Send bill/attachment

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ subject = "This is the subject"
 
 yag = yagmail.SMTP(user=sender, password=os.getenv('EMAIL_PASSWORD'))
 
-def send_email(receiver, name):
+
+def send_email(receiver, name, amount, filepath):
     contents = [f"""
-    Hello {name}
-    Here's the email contents!
-    """, 'text.txt']
+        Hello {name}, you must pay {amount}
+        The bill is attached!
+        """, filepath]
 
     yag.send(to=receiver, subject=subject, contents=contents)
     print("Email sent")
@@ -23,4 +24,4 @@ df = pandas.read_csv('contacts.csv')
 print(df)
 
 for index, row in df.iterrows():
-    send_email(row['email'], row['name'])
+    send_email(row['email'], row['name'], row['amount'], 'Bills/' + row['filepath'])
